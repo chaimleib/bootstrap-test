d86caa1556bd523932bf4fc83012b6a1eb71b164
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-08 11:27:42 -0700

    upd: public

--- public/assets/application-9a759a54059485262979a9240be60664b6ac6d38430a8f0ae48ea9301264f5b1.js
+++ public/assets/application-9a759a54059485262979a9240be60664b6ac6d38430a8f0ae48ea9301264f5b1.js
@@ -11613,6 +11613,10 @@ return jQuery;
   };
 
 }).call(this);
+(function() {
+
+
+}).call(this);
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
