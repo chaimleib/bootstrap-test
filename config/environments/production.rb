d6fea072633474d6bf2053a52bd170dacccb0e35
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-08 11:16:40 -0700

    production: precompile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
+  # This replaces the deprecated config.serve_static_assets option.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
@@ -29,7 +30,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
